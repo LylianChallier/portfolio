8c01e65ecb2086a1999d2502eb11cb5b29665162
Lylian Challier <lylianchallier@MacBook-Air-M2-DTY-de-Lylian.local>
2026-01-17 17:57:17 +0100

    react and three.js compatibility

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // Remplace 'portfolio' par le nom exact de ton repo GitHub
+  base: '/portfolio/',
+  resolve: {
+    dedupe: ['react', 'react-dom'],
+  },
 })
